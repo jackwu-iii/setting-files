9e14b5e4548c2f16ba534dd0edf527a8cd0534f0
jackwu-iii <jackwu@iii.org.tw>
2023-11-14 11:42:39 +0800

    update nodejs 18, yarn, install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,22 +1,38 @@
 #!/bin/sh
 
-# install python3
-sudo apt-get update
-sudo apt-get install vim python3 python3-venv python3-pip python-virtualenv zsh
+## install python3
+sudo apt update
+sudo apt install -y vim python3 python3-venv python3-pip python3-virtualenv zsh
 
-# install nodejs
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+## install git-lfs, git-flow
+sudo apt install -y git-lfs git-flow
+
+## step of install nodejs
+### 1. add key
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+### 2. build deb
+NODE_MAJOR=18
+ARCH=amd64
+echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+### 3. install nodejs & git-jump
+sudo apt update && sudo apt install -y nodejs
+sudo apt install -y build-essential
 npm install -g git-jump
 
 # install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+#echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+#sudo apt update && sudo apt install yarn
+sudo corepack enable
+yarn init -2
 
-# install yarn completion
-yarn global add yarn-completions
+# install yarn completion (deprecated)
+# yarn global add yarn-completions
 
+# TODO : pending
 # install diff-so-dancy
-sudo apt-get install -y diff-so-fancy
+#sudo apt install -y diff-so-fancy
